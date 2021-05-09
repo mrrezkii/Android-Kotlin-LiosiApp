1db8b0a2b8411adb5e384e279cdc698d98947070
nando <muhammad.rezki.ananda@gmail.com>
2021-05-09 10:48:33 +0700

    Configure Fragment Muscle

--- app/src/main/java/com/gemastik/liosiapp/data/view/fragment/MuscleFragment.kt
+++ app/src/main/java/com/gemastik/liosiapp/data/view/fragment/MuscleFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.gemastik.liosiapp.R
 import com.gemastik.liosiapp.databinding.FragmentMuscleBinding
 
 class MuscleFragment : Fragment() {
@@ -17,6 +20,49 @@ class MuscleFragment : Fragment() {
     ): View? {
         binding = FragmentMuscleBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupView()
+    }
+
+    private fun setupView() {
+        binding.btnBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        binding.scrollContainer.menuMuscle1.tvTitle.text = getString(R.string.menu_muscle_1)
+        binding.scrollContainer.menuMuscle1.ivBackground.setImageDrawable(
+            ContextCompat.getDrawable(
+                requireContext(),
+                R.drawable.balasana
+            )
+        )
+
+        binding.scrollContainer.menuMuscle2.tvTitle.text = getString(R.string.menu_muscle_2)
+        binding.scrollContainer.menuMuscle2.ivBackground.setImageDrawable(
+            ContextCompat.getDrawable(
+                requireContext(),
+                R.drawable.viparita_korani
+            )
+        )
+
+        binding.scrollContainer.menuMuscle3.tvTitle.text = getString(R.string.menu_muscle_3)
+        binding.scrollContainer.menuMuscle4.ivBackground.setImageDrawable(
+            ContextCompat.getDrawable(
+                requireContext(),
+                R.drawable.makarasona
+            )
+        )
+
+        binding.scrollContainer.menuMuscle4.tvTitle.text = getString(R.string.menu_muscle_4)
+        binding.scrollContainer.menuMuscle4.ivBackground.setImageDrawable(
+            ContextCompat.getDrawable(
+                requireContext(),
+                R.drawable.savasana
+            )
+        )
     }
 
 }
