7a0b2235bfc4a7e374a37b432ba3d4df90880302
nando <muhammad.rezki.ananda@gmail.com>
2021-05-09 09:55:43 +0700

    Configure Fragment Premium

--- app/src/main/java/com/gemastik/liosiapp/data/view/fragment/PremiumFragment.kt
+++ app/src/main/java/com/gemastik/liosiapp/data/view/fragment/PremiumFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.gemastik.liosiapp.R
 import com.gemastik.liosiapp.databinding.FragmentPremiumBinding
 
 class PremiumFragment : Fragment() {
@@ -17,6 +20,50 @@ class PremiumFragment : Fragment() {
     ): View? {
         binding = FragmentPremiumBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupView()
+    }
+
+    private fun setupView() {
+        binding.btnBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        binding.scrollContainer.menuPremium1.tvTitle.text = getString(R.string.menu_premium_1)
+        binding.scrollContainer.menuPremium1.ivBackground.setImageDrawable(
+            ContextCompat.getDrawable(
+                requireContext(),
+                R.drawable.bg_breathe_positif
+            )
+        )
+
+        binding.scrollContainer.menuPremium2.tvTitle.text = getString(R.string.menu_premium_2)
+        binding.scrollContainer.menuPremium2.ivBackground.setImageDrawable(
+            ContextCompat.getDrawable(
+                requireContext(),
+                R.drawable.bg_mood_todo
+            )
+        )
+
+        binding.scrollContainer.menuPremium3.tvTitle.text = getString(R.string.menu_premium_3)
+        binding.scrollContainer.menuPremium3.ivBackground.setImageDrawable(
+            ContextCompat.getDrawable(
+                requireContext(),
+                R.drawable.bg_home_positif
+            )
+        )
+
+        binding.scrollContainer.menuPremium4.tvTitle.text = getString(R.string.menu_premium_4)
+        binding.scrollContainer.menuPremium4.ivBackground.setImageDrawable(
+            ContextCompat.getDrawable(
+                requireContext(),
+                R.drawable.bg_breathe_positif
+            )
+        )
+
     }
 
 }
