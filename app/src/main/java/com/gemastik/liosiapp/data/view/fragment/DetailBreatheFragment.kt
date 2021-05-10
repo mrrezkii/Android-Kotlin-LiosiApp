b7a8ec3024e9927a59ac06e74748cf86d54d9532
nando <muhammad.rezki.ananda@gmail.com>
2021-05-10 08:30:55 +0700

    Add fragment detail breathe

---
+++ app/src/main/java/com/gemastik/liosiapp/data/view/fragment/DetailBreatheFragment.kt
@@ -0,0 +1,21 @@
+package com.gemastik.liosiapp.data.view.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.gemastik.liosiapp.databinding.FragmentBreatheBinding
+
+class DetailBreatheFragment : Fragment() {
+
+    private lateinit var binding: FragmentBreatheBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBreatheBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
